8a63b5c301560c822c2dbcd29aef785bb5694b72
Marcos Tzuc Cen <marcos@virtualia.com>
2023-04-28 22:24:34 -0600

    Actualizando librerias

--- arduino_849115/docs/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['arduino_20led_20matrix_20library_214',['Arduino LED Matrix Library',['../index.html',1,'']]]
-];
--- arduino_849115/docs/search/pages_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['revision_20history_223',['Revision History',['../page_revision_history.html',1,'']]]
-];
